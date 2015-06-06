691aeb78246063375aa5ee67687f70db25170d8d
Parvez shaikh <parvezshaikhj@gmail.com>
2015-06-07 00:55:43 +0530

    Scrolltop functionality added for logo and text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 $(window).scroll(function(){
 	
 	var wScroll = $(this).scrollTop();
-	console.log(wScroll);
+	// console.log(wScroll);
 
+	$('.logo').css({
+		'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+	});
+
+	$('.descriptive-text p').css({
+		'transform' : 'translate(0px, -'+ wScroll /10 +'%)'
+	});
 });
