09ca08b9c7ae0a359b75347636f30743b0456dfc
Parvez shaikh <parvezshaikhj@gmail.com>
2015-06-07 13:43:27 +0530

    Script implemented on Figure on scroll effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,14 @@ $(window).scroll(function(){
 	$('.descriptive-text p').css({
 		'transform' : 'translate(0px, -'+ wScroll /20 +'%)'
 	});
+
+	//Calculating when the scrollTop is more than cloth-pics
+	//If wScroll is more than the top offset of cloth-pics, perform the below action
+	if(wScroll > $('.clothes-pics').offset().top) {
+		// console.log('hihih');
+		$('.clothes-pics figure').each(function(){
+			$('.clothes-pics figure').addClass('.is-showing');
+			//is-showing is coded in style.scss
+		});
+	}
 });
